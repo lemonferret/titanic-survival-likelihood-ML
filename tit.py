77774374d83e8d887e1cb927754073c5fe05c2c7
Anna Liski <liskianna@gmail.com>
2020-08-09 18:23:18 +0300

    class and sex cross correlation start

--- tit.py
+++ tit.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-#test
+
 def readdata():
 	data = pd.read_csv("train.csv", delimiter=',', skiprows=0)
 
@@ -37,16 +37,24 @@ def p_sex(surv, sex) :
 			if surv[i]==1: P[1, 0] += 1		
 	return P #survived, all
 	
-	
+def p_class_sex(surv, pclass, sex):
+	Pf  = np.transpose(np.array([[1 ,2 ,3], [0, 0 , 0], [0, 0, 0]])) #female: class, survived, all
+	Pm  = np.transpose(np.array([[1 ,2 ,3], [0, 0 , 0], [0, 0, 0]])) #male: class, survived, all
+	#for i, n in enumerate(sex):
+	#	if n=='female': 
+	#		pclass[i]-1
+	#	if n=='male':
+		
 def main():
 	pid, surv, pclass, name, sex, age, sib, parch, ticket, fare, cabin, embark = readdata()
 	P_CLASS = p_pclass(surv, pclass)
 	print("\nLikelihood of survival in 1. class: " + str(P_CLASS[0, 1]/P_CLASS[0, 2]))
 	print("Likelihood of survival in 2. class: " + str(P_CLASS[1, 1]/P_CLASS[1, 2]))
-	print("Likelihood of survival in 3. class: " + str(P_CLASS[2, 1]/P_CLASS[2, 2])+"\n")
+	print("Likelihood of survival in 3. class: " + str(P_CLASS[2, 1]/P_CLASS[2, 2]))
 	P_SEX = p_sex(surv, sex)
 	print("\nLikelihood of survival F: " + str(P_SEX[0, 0]/P_SEX[0, 1]))
 	print("Likelihood of survival M: " + str(P_SEX[1, 0]/P_SEX[1, 1])+"\n")
+	P_CLASS_SEX = p_class_sex(surv, pclass, sex)
 	
 	#print("\nLikelihood of survival F in the 1 calss: " + str(P_SEX[0, 0]/P_SEX[0, 1]))
 	#print("Likelihood of survival M: " + str(P_SEX[1, 0]/P_SEX[1, 1])+"\n")
